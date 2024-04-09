b17b66df4563a0884c32c6aee69905fac667f16d
antoine-2beco <mail@antoinedebeco.com>
2024-04-09 15:08:05 +0200

    client and server

--- src/client.c
+++ src/client.c
@@ -6,22 +6,41 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:58:07 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/04/09 11:39:50 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/04/09 15:06:53 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/client.h"
 
-void	error(char *error_msg)
+void    send_signal(int pid, char c)
 {
-	ft_printf("Error\n%s\n", error_msg);
-	exit(EXIT_FAILURE);
+    int bit;
+
+    bit = 0;
+    while (bit < 8)
+    {
+        if ((c &(0x01 << bit)) != 0)
+            kill(pid, SIGUSR1);
+        else
+            kill(pid, SIGUSR2);
+        usleep(100);
+        bit++;
+    }
 }
 
 int	main(int argc, char *argv[])
 {
+	int	i;
+	int	pid;
+
 	if (argc != 3)
-		error("Arguments Error");
-	argv++;
+		exit(EXIT_FAILURE);
+	i = 0;
+	pid = atoi(argv[1]);
+	while (argv[2][i])
+	{
+		send_signal(pid, argv[2][i]);
+		i++;
+	}
 	return (0);
 }
--- src/server.c
+++ src/server.c
@@ -6,18 +6,37 @@
 /*   By: ade-beco <ade-beco@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 10:58:09 by ade-beco          #+#    #+#             */
-/*   Updated: 2024/04/09 12:36:44 by ade-beco         ###   ########.fr       */
+/*   Updated: 2024/04/09 14:59:51 by ade-beco         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/server.h"
 
+void	sig_handler(int signal)
+{
+	static int bit;
+    static int i;
+
+    if (signal == SIGUSR1)
+        i |= (0x01 << bit);
+    bit++;
+    if (bit == 8)
+    {
+        ft_printf("%c", i);
+        bit = 0;
+        i = 0;
+    }
+}
+
 int	main(void)
 {
-	struct	sigaction sa;
-	int		pid;
+	int					pid;
 
 	pid = getpid();
-	ft_printf("PID PROCCES : %i\n", pid);
+	ft_printf("PID : %i\n", pid);
+	signal(SIGUSR1, sig_handler);
+	signal(SIGUSR2, sig_handler);
+	while (1)
+		pause();
 	return (0);
 }
